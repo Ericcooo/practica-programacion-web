c5bd75cdf909ed23a5b6392feb0fce4969263692
Erick Adrian <e.a.s.r.1jv@gmail.com>
2024-04-28 23:39:21 -0700

    practica imc

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const btnCalcular = document.getElementById('btnCalcular');
+const btnLimpiar = document.getElementById('btnLimpiar');
+
+btnLimpiar.addEventListener('click', function btnLimpiar(){
+
+    document.getElementById('peso'). value =""
+    document.getElementById('altura'). value = ""
+    document.getElementById('resultado').value = ""
+});
+
+btnCalcular.addEventListener('click',function calcular(){
+
+let peso = document.getElementById('peso'). value ;
+let altura = document.getElementById('altura'). value ;
+let imc = peso/(altura*altura);
+document.getElementById('resultado').value = imc.toFixed(1);
+}
+);
